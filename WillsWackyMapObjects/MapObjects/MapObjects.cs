6712b71a8cdc1fc2e4cb20245b023af7769538a5
willuwontu <willuwontu3@gmail.com>
2022-01-24 17:36:18 -0800

    Initial commit

---
+++ WillsWackyMapObjects/MapObjects/MapObjects.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using MapsExt;
+using MapsExt.MapObjects;
+using UnboundLib;
+using UnityEngine;
+using WWMO.MonoBehaviours;
+
+namespace WWMO.MapObjects
+{
+    #region Water Objects
+    #region Water
+    public class Water : SpatialMapObject
+    {
+    }
+
+    [MapObjectSpec(typeof(Water))]
+    public static class WaterSpec
+    {
+        [MapObjectPrefab] 
+        public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground").GetOrAddComponent<WaterMono>().gameObject;
+
+        [MapObjectSerializer]
+        public static void Serialize(GameObject instance, Water target)
+        {
+            SpatialSerializer.Serialize(instance, target);
+        }
+
+        [MapObjectDeserializer]
+        public static void Deserialize(Water data, GameObject target)
+        {
+            SpatialSerializer.Deserialize(data, target);
+            target.GetOrAddComponent<WaterMono>();
+        }
+    }
+
+    #endregion
+
+    #region Acid
+
+    public class Acid : SpatialMapObject
+    {
+    }
+
+    [MapObjectSpec(typeof(Acid))]
+    public static class AcidSpec
+    {
+        [MapObjectPrefab]
+        public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground").GetOrAddComponent<AcidMono>().gameObject;
+
+        [MapObjectSerializer]
+        public static void Serialize(GameObject instance, Acid target)
+        {
+            SpatialSerializer.Serialize(instance, target);
+        }
+
+        [MapObjectDeserializer]
+        public static void Deserialize(Acid data, GameObject target)
+        {
+            SpatialSerializer.Deserialize(data, target);
+            target.GetOrAddComponent<AcidMono>();
+        }
+    }
+
+    #endregion
+
+    #region Lava
+
+    public class Lava : SpatialMapObject
+    {
+    }
+
+    [MapObjectSpec(typeof(Lava))]
+    public static class LavaSpec
+    {
+        [MapObjectPrefab]
+        public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground").GetOrAddComponent<LavaMono>().gameObject;
+
+        [MapObjectSerializer]
+        public static void Serialize(GameObject instance, Lava target)
+        {
+            SpatialSerializer.Serialize(instance, target);
+        }
+
+        [MapObjectDeserializer]
+        public static void Deserialize(Lava data, GameObject target)
+        {
+            SpatialSerializer.Deserialize(data, target);
+            target.GetOrAddComponent<LavaMono>();
+        }
+    }
+
+    #endregion
+
+    #region Space
+
+    public class Space : SpatialMapObject
+    {
+    }
+
+    [MapObjectSpec(typeof(Space))]
+    public static class SpaceSpec
+    {
+        [MapObjectPrefab]
+        public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground").GetOrAddComponent<SpaceMono>().gameObject;
+
+        [MapObjectSerializer]
+        public static void Serialize(GameObject instance, Space target)
+        {
+            SpatialSerializer.Serialize(instance, target);
+        }
+
+        [MapObjectDeserializer]
+        public static void Deserialize(Space data, GameObject target)
+        {
+            SpatialSerializer.Deserialize(data, target);
+            target.GetOrAddComponent<SpaceMono>();
+        }
+    }
+
+    #endregion
+    #endregion
+}
---
+++ WillsWackyMapObjects/WillsWackyMapObjects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using MapsExt;
+using UnboundLib;
+
+namespace WWMO
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.willuwontu.rounds.managers", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class WillsWackyMapObjects : BaseUnityPlugin
+    {
+        private const string ModId = "com.willuwontu.rounds.MapObjects";
+        private const string ModName = "Will's Wacky Map Objects";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+
+        public static WillsWackyMapObjects instance { get; private set; }
+
+        void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        void Start()
+        {
+            Unbound.RegisterCredits(ModName, new string[] { "willuwontu" }, new string[] { "github", "Ko-Fi" }, new string[] { "https://github.com/willuwontu/wills-wacky-cards", "https://ko-fi.com/willuwontu" });
+
+            instance = this;
+
+            var assembly = Assembly.GetCallingAssembly();
+            this.ExecuteAfterSeconds(0.1f, () => { MapsExtended.instance.RegisterMapObjectsAction?.Invoke(assembly); });
+        }
+    }
+}

e51c83c43a4a78f2c0247ac88cd28686dedaa311
willuwontu <willuwontu3@gmail.com>
2023-05-16 21:22:58 -0700

    Revive of old files

---
+++ WillsWackyMapObjects/MapObjects/MapObjects.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using MapsExt;
+using MapsExt.MapObjects;
+using UnboundLib;
+using UnityEngine;
+using WWMO.MonoBehaviours;
+
+namespace WWMO.MapObjects
+{
+		#region Liquids
+        #region Water
+        public class Water : SpatialMapObject
+        {
+        }
+
+        [MapObjectSpec(typeof(Water))]
+        public static class WaterSpec
+        {
+            [MapObjectPrefab]
+            public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground");
+
+            [MapObjectSerializer]
+            public static void Serialize(GameObject instance, Water target)
+            {
+                SpatialSerializer.Serialize(instance, target);
+            }
+
+            [MapObjectDeserializer]
+            public static void Deserialize(Water data, GameObject target)
+            {
+                SpatialSerializer.Deserialize(data, target);
+                target.GetOrAddComponent<WaterMono>();
+            }
+        }
+
+        #endregion
+
+        #region Acid
+
+        public class Acid : SpatialMapObject
+        {
+        }
+
+        [MapObjectSpec(typeof(Acid))]
+        public static class AcidSpec
+        {
+            [MapObjectPrefab]
+            public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground");
+
+            [MapObjectSerializer]
+            public static void Serialize(GameObject instance, Acid target)
+            {
+                SpatialSerializer.Serialize(instance, target);
+            }
+
+            [MapObjectDeserializer]
+            public static void Deserialize(Acid data, GameObject target)
+            {
+                SpatialSerializer.Deserialize(data, target);
+                target.GetOrAddComponent<AcidMono>();
+            }
+        }
+
+        #endregion
+
+        #region Lava
+
+        public class Lava : SpatialMapObject
+        {
+        }
+
+        [MapObjectSpec(typeof(Lava))]
+        public static class LavaSpec
+        {
+            [MapObjectPrefab]
+            public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground");
+
+            [MapObjectSerializer]
+            public static void Serialize(GameObject instance, Lava target)
+            {
+                SpatialSerializer.Serialize(instance, target);
+            }
+
+            [MapObjectDeserializer]
+            public static void Deserialize(Lava data, GameObject target)
+            {
+                SpatialSerializer.Deserialize(data, target);
+                target.GetOrAddComponent<LavaMono>();
+            }
+        }
+
+        #endregion
+
+        #region Space
+
+        public class Space : SpatialMapObject
+        {
+        }
+
+        [MapObjectSpec(typeof(Space))]
+        public static class SpaceSpec
+        {
+            [MapObjectPrefab]
+            public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground");
+
+            [MapObjectSerializer]
+            public static void Serialize(GameObject instance, Space target)
+            {
+                SpatialSerializer.Serialize(instance, target);
+            }
+
+            [MapObjectDeserializer]
+            public static void Deserialize(Space data, GameObject target)
+            {
+                SpatialSerializer.Deserialize(data, target);
+                target.GetOrAddComponent<SpaceMono>();
+            }
+        }
+
+        #endregion
+
+        #region ForceZone
+
+        public class ForceZone : SpatialMapObject
+        {
+        }
+
+        [MapObjectSpec(typeof(ForceZone))]
+        public static class ForceZoneSpec
+        {
+            [MapObjectPrefab]
+            public static GameObject Prefab => MapObjectManager.LoadCustomAsset<GameObject>("Ground");
+
+            [MapObjectSerializer]
+            public static void Serialize(GameObject instance, ForceZone target)
+            {
+                SpatialSerializer.Serialize(instance, target);
+            }
+
+            [MapObjectDeserializer]
+            public static void Deserialize(ForceZone data, GameObject target)
+            {
+                SpatialSerializer.Deserialize(data, target);
+                target.GetOrAddComponent<ForceZoneMono>();
+            }
+        }
+
+        #endregion
+
+        #endregion 
+}
---
+++ WillsWackyMapObjectsEditor/MapObjects/MapObjects.cs
@@ -0,0 +1,89 @@
+﻿using MapsExt.MapObjects;
+using MapsExt.Editor;
+using MapsExt.Editor.MapObjects;
+using UnityEngine;
+
+namespace WWMO.MapObjects
+{
+    #region Water Objects
+    #region Water
+
+    [EditorMapObjectSpec(typeof(MapObjects.Water), "Water", "Liquids")]
+    public static class EditorWater
+    {
+        [EditorMapObjectPrefab] public static GameObject Prefab => MapObjects.WaterSpec.Prefab;
+
+        [EditorMapObjectSerializer]
+        public static SerializerAction<MapObjects.Water> Serialize => EditorSpatialSerializer.BuildSerializer<MapObjects.Water>(MapObjects.WaterSpec.Serialize);
+
+        [EditorMapObjectDeserializer]
+        public static DeserializerAction<MapObjects.Water> Deserialize => EditorSpatialSerializer.BuildDeserializer<MapObjects.Water>(MapObjects.WaterSpec.Deserialize);
+    }
+
+    #endregion
+
+    #region Acid
+
+    [EditorMapObjectSpec(typeof(MapObjects.Acid), "Acid", "Liquids")]
+    public static class EditorAcid
+    {
+        [EditorMapObjectPrefab] public static GameObject Prefab => MapObjects.AcidSpec.Prefab;
+
+        [EditorMapObjectSerializer]
+        public static SerializerAction<MapObjects.Acid> Serialize => EditorSpatialSerializer.BuildSerializer<MapObjects.Acid>(MapObjects.AcidSpec.Serialize);
+
+        [EditorMapObjectDeserializer]
+        public static DeserializerAction<MapObjects.Acid> Deserialize => EditorSpatialSerializer.BuildDeserializer<MapObjects.Acid>(MapObjects.AcidSpec.Deserialize);
+    }
+
+    #endregion
+
+    #region Lava
+
+    [EditorMapObjectSpec(typeof(MapObjects.Lava), "Lava", "Liquids")]
+    public static class EditorLava
+    {
+        [EditorMapObjectPrefab] public static GameObject Prefab => MapObjects.LavaSpec.Prefab;
+
+        [EditorMapObjectSerializer]
+        public static SerializerAction<MapObjects.Lava> Serialize => EditorSpatialSerializer.BuildSerializer<MapObjects.Lava>(MapObjects.LavaSpec.Serialize);
+
+        [EditorMapObjectDeserializer]
+        public static DeserializerAction<MapObjects.Lava> Deserialize => EditorSpatialSerializer.BuildDeserializer<MapObjects.Lava>(MapObjects.LavaSpec.Deserialize);
+    }
+
+    #endregion
+
+    #region Space
+
+    [EditorMapObjectSpec(typeof(MapObjects.Space), "Space", "Liquids")]
+    public static class EditorSpace
+    {
+        [EditorMapObjectPrefab] public static GameObject Prefab => MapObjects.SpaceSpec.Prefab;
+
+        [EditorMapObjectSerializer]
+        public static SerializerAction<MapObjects.Space> Serialize => EditorSpatialSerializer.BuildSerializer<MapObjects.Space>(MapObjects.SpaceSpec.Serialize);
+
+        [EditorMapObjectDeserializer]
+        public static DeserializerAction<MapObjects.Space> Deserialize => EditorSpatialSerializer.BuildDeserializer<MapObjects.Space>(MapObjects.SpaceSpec.Deserialize);
+    }
+
+    #endregion
+
+    #region Space
+
+    [EditorMapObjectSpec(typeof(MapObjects.ForceZone), "Force Zone", "Liquids")]
+    public static class EditorForceZone
+    {
+        [EditorMapObjectPrefab] public static GameObject Prefab => MapObjects.ForceZoneSpec.Prefab;
+
+        [EditorMapObjectSerializer]
+        public static SerializerAction<MapObjects.ForceZone> Serialize => EditorSpatialSerializer.BuildSerializer<MapObjects.ForceZone>(MapObjects.ForceZoneSpec.Serialize);
+
+        [EditorMapObjectDeserializer]
+        public static DeserializerAction<MapObjects.ForceZone> Deserialize => EditorSpatialSerializer.BuildDeserializer<MapObjects.ForceZone>(MapObjects.ForceZoneSpec.Deserialize);
+    }
+
+    #endregion
+    #endregion
+}
